4c9eda115c7119c7b30249bcdb92ea5bb9810c1e
g1ng3r-bread-man <alexkerle21@gmail.com>
2025-07-13 18:44:22 +1000

    made the game take less resources, limited it to 60 fps

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ import pygame
 
 def main():
     pygame.init
+    dt = 0
     print("Starting Asteroids!")
     print(f"""Screen width: {SCREEN_WIDTH}
 Screen height: {SCREEN_HEIGHT}""")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        pygame.time.Clock().tick(60)
+        frame = pygame.time.Clock().tick(60)
+        dt = frame / 1000
 
 
 
