7cc5d98afc521407539504c4f8fadaba991fa4a1
g1ng3r-bread-man <alexkerle21@gmail.com>
2025-07-16 15:03:31 +1000

    made containers for drawables and updatables, and now it updates/draws everything in them

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from constants import *
-from player import Player
+from player import *
 import pygame
 
 
 def main():
     pygame.init
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     dt = 0
     print("Starting Asteroids!")
     print(f"""Screen width: {SCREEN_WIDTH}
@@ -17,8 +20,9 @@ Screen height: {SCREEN_HEIGHT}""")
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        playerchar.update(dt)
-        playerchar.draw(screen)
+        updatable.update(dt)
+        for drawble in drawable:
+            drawble.draw(screen)
         pygame.display.flip()
         pygame.time.Clock().tick(60)
         frame = pygame.time.Clock().tick(60)
--- player.py
+++ player.py
@@ -5,6 +5,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.containers = ()
 
         # in the player class
     def triangle(self):
